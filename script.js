b417d4c1b4e5f640e97fc4c4863ab8d8dc4e63b6
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-24 11:51:30 -0400

    create functions based on button value

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ const buttonsEl = document.querySelectorAll("button");
 // will add eventListener for each btn
 for (let i = 0; i < buttonsEl.length; i++) {
   buttonsEl[i].addEventListener("click", () => {
-    console.log(buttonsEl[i].textContent);
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } 
+    //else display the clicked num
+    else {
+      appendValue(buttonValue);
+    }
   });
 }
 
+//clear result
+function clearResult(){
 
+};
+
+//calculate result
+function calculateResult(){
+
+};
+
+//display the clicked num
+function appendValue() {};
